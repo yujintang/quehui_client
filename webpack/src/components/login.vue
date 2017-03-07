4f1427495221c4bbb4ff65513aba29f1d292c869
陆俞祁 <lyq19931004@gmail.com>
2017-03-07 17:52:18 +0800

    修改

--- webpack/src/components/login.vue
+++ webpack/src/components/login.vue
@@ -18,7 +18,7 @@
         // 组件创建完后获取数据，
         // 此时 data 已经被 observed 了
         // this.fetchData()
-        axios.get('qh_api/api/corp',{})
+        axios.get('../qh_api/api/corp',{})
         .then(function (res) {
             console.log(res)
         })
